77a14c4970a8ed5627b30a99031660ef3f906d70
vammar <vammar@scriptrx.com>
2015-04-29 22:02:45 -0400

    process.env.REDISTOGO_URL

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.use( session({
     secret: 'cookie_secret',
     name:   'kaas',
     store:  new RedisStore({
-        host: '127.0.0.1',
+        host: process.env.REDISTOGO_URL,
         port: 6379
     }),
     proxy:  true,
